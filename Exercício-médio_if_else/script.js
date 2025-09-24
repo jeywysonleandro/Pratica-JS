372a041cfd22950841504defce1776e6202c61d4
jeywyson leandro <jeyads20@gmail.com>
2025-09-24 00:52:25 -0300

    calculadora simples

--- Exercício-médio_if_else/script.js
+++ Exercício-médio_if_else/script.js
@@ -1,3 +1,5 @@
+//1. Classificação de notas
+
 let nota = 51
 
 if (nota < 40) {
@@ -10,4 +12,31 @@ else if (nota >= 40 && nota <= 59) {
 
 else {
     console.log("aprovado");
+}
+
+//2. Calculadora simples
+
+let num1 = 50;
+let num2 = 20;
+
+let operacao = "-"
+
+if (operacao === "+") {
+    console.log(num1 + num2);
+}
+
+else if (operacao === "-") {
+    console.log(num1 - num2);
+}
+
+else if (operacao === "*") {
+    console.log(num1 * num2);
+}
+
+else if (operacao === "/") {
+    console.log(num1 / num2);
+}
+
+else {
+    console.log("Operação inválida");
 }
