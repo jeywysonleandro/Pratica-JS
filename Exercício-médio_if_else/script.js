47ac2522d420b79756ee14e8b063bfe14598e971
jeywyson leandro <jeyads20@gmail.com>
2025-09-24 00:37:50 -0300

    classificar nota do aluno

---
+++ Exercício-médio_if_else/script.js
@@ -0,0 +1,13 @@
+let nota = 51
+
+if (nota < 40) {
+    console.log("reprovado");
+}
+
+else if (nota >= 40 && nota <= 59) {
+    console.log("recuperação");
+}
+
+else {
+    console.log("aprovado");
+}
