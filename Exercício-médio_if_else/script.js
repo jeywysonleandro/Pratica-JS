464dee34aeffcdc7bb6851167350309577a4bec5
jeywyson leandro <jeyads20@gmail.com>
2025-09-24 22:45:14 -0300

    questao 3 media

--- Exercício-médio_if_else/script.js
+++ Exercício-médio_if_else/script.js
@@ -45,12 +45,18 @@ else {
 
 let idade = 20
 
-if (idade >= 18) {
-    console.log("Adulto");
-}
-
-else if (idade <= 13) {
+if (idade <= 12) {
     console.log("Criança");
 }
 
-else if (idade >=)
+else if (idade <=17) {
+    console.log("Adolescente");
+}
+
+else if (idade <=59) {
+    console.log("Adulto");
+}
+
+else {
+    console.log("Idoso");
+}
