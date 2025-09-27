07c815482e39f0a8f29adafab0a6e1d592d7388f
jeywyson leandro <jeyads20@gmail.com>
2025-09-27 00:20:59 -0300

    apaguei a 2 questao que era errada e add a certa

--- Exercício-difícil_if_else/script.js
+++ Exercício-difícil_if_else/script.js
@@ -26,22 +26,8 @@ else {
 
 console.log("Renda líquida: R$ " + (renda - imposto).toFixed(2));
 
-//2 – Cálculo de Faixa Etária Detalhada
+//2 – Sistema de notas com classificação múltipla
 
-let idade = 21
 
-if (idade <= 12) {
-    console.log("Criança");
-}
 
-else if (idade <= 17) {
-    console.log("Adolescente");
-}
-
-else if (idade <= 59) {
-    console.log("Adulto");
-} 
-
-else {
-    console.log("Idoso")
-}
+//
