3d273b0980517292f27beaf8fd8e52c97b775360
jeywyson leandro <jeyads20@gmail.com>
2025-09-27 00:28:31 -0300

    Sistema de notas com classificação múltipla

--- Exercício-difícil_if_else/script.js
+++ Exercício-difícil_if_else/script.js
@@ -28,6 +28,26 @@ console.log("Renda líquida: R$ " + (renda - imposto).toFixed(2));
 
 //2 – Sistema de notas com classificação múltipla
 
+let nota = 90;
 
+if (nota >=90) {
+    console.log("A");
+}
+
+else if (nota >=80) {
+    console.log("B");
+}
+
+else if (nota >=70) {
+    console.log("C");
+}
+
+else if (nota >=60) {
+    console.log("D");
+}
+
+else {
+    console.log("F");
+}
 
 //
