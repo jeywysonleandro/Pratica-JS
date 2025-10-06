0a3f2e43111d1d05542ffdb3a5b30255d8209ec1
Jeywyson Leandro <jeyads20@gmail.com>
2025-10-06 00:03:00 -0300

    exercicio 1 basico

---
+++ Exercícios-Objetos-basico/script.js
@@ -0,0 +1,6 @@
+const pessoa = {
+    nome: "Jeywyson",
+    idade: 21,
+    cidade: 'Caruaru'    
+};
+console.log(`Olá meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e moro em ${pessoa.cidade}.`);
