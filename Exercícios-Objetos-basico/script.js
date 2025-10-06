95d1631a1fe95da7bc4ac46f602cf60440475606
Jeywyson Leandro <jeyads20@gmail.com>
2025-10-06 00:12:41 -0300

    exercicio 2 basico

--- Exercícios-Objetos-basico/script.js
+++ Exercícios-Objetos-basico/script.js
@@ -1,6 +1,25 @@
+//1. Criando um objeto pessoa
+
 const pessoa = {
     nome: "Jeywyson",
     idade: 21,
     cidade: 'Caruaru'    
 };
 console.log(`Olá meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e moro em ${pessoa.cidade}.`);
+
+//2. Alterando valores
+
+const carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2020
+};
+
+console.log('Objeto antes');
+console.log(carro);
+
+carro.ano = 2022
+carro.cor = "Prata"
+
+console.log('\nObjeto DEPOIS:');
+console.log(carro);
