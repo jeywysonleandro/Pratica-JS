c65bc07f5cd2bd7857404d43f24a79bd5b4a9385
Jeywyson Leandro <jeyads20@gmail.com>
2025-10-08 01:17:05 -0300

    questao 2

--- Exercícios-Objetos-medio/script.js
+++ Exercícios-Objetos-medio/script.js
@@ -12,3 +12,22 @@ function atualizarPreco(obj, novoPreco) {
 }
 
 console.log(atualizarPreco(produto, 4000));
+
+//Questão 2 – Soma de valores
+
+const carrinho = {
+    item1: 50,
+    item2: 30,
+    item3: 20
+};
+
+function totalCarrinho(obj) {
+    let total = 0;
+    for (let key in obj) {
+        total += obj[key];
+    }
+    return total;
+}
+
+console.log(totalCarrinho(carrinho));
+
